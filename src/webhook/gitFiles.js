a02351e0015d275e04c193c86355dfd593ccb8ac
Lasse <lassetange@gmail.com>
2017-07-01 16:46:42 +0200

    Checking web hook for files

--- src/webhook/gitFiles.js
+++ src/webhook/gitFiles.js
@@ -1,1 +1,27 @@
-// Check if commit has files or too many
+import { allowedFiles } from './webhookUtils';
+
+const gitFiles = (req, res, next) => {
+  const commits = req.body.commits;
+  const files = [];
+
+  if (!commits) {
+    return res.send('No commited files.'); // Initial test web hook
+  }
+
+  // Check if files exists
+  for (var i = 0; i < commits.length; i++) {
+    const commitFiles = allowedFiles(commits[i]);
+
+    if (commitFiles.length > 0) {
+      files.push(commitFiles);
+    }
+  }
+
+  if (files.length === 0) {
+    return res.send('No files to lint.');
+  }
+
+  return next();
+}
+
+export default gitFiles;
